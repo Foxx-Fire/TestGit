dd5a99457fe3b59befd8e7d71d74ce925bd02fe7
Olga Mikhailova <foxx_fire@mail.ru>
2025-06-11 18:34:05 +0300

    Initial Commit

---
+++ TestGit/TestGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitApp.swift
+//  TestGit
+//
+//  Created by FoxxFire on 11.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
